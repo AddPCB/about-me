104afa8e2bff36f7a183a46a3663ab8b3443f89e
Peter Brightmore <P.C.Brightmore@gmail.com>
2023-01-26 10:52:46 +0000

    fix question code and add question 6 and 7

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Lab 2
+# about me
+
+- Created an About Me guessing game using HTML, CSS, and JavaScript.
+
+- Added a short biography, education history, job experience, and goals to the HTML page.
+
+- Prompted the user a total of 5 yes or no questions and validated the user input to make sure it is case sensitive.
+
+- Added alert messages to notify the user if they answered the question correctly.
+
+- Asked the user their name through a prompt().
+
+- Displayed the user's name back to them through a custom greeting and in a final message.
+
+- Added lighthouse accessibility score for completed lab02 about page:
+
+![Lighthouse score](images/lighthouse1.png)
--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+/* eslint-disable eqeqeq */
 "use strict";
 function titleCase(str) {
-  str = str.split(' ');
+  str = str.split(" ");
   let title ="";
   let result = [];
   for(let i = 0; i < str.length; i++){
     title = str[i].toLowerCase();
     result.push(title[0].toUpperCase()+title.slice(1));
   }
-  return result.join(' ');
+  return result.join(" ");
 }
+// Create a variable to store the correct answers
+let correctAnswers = 0;
+
+// Ask user name
 let myName = prompt ("What is your name?");
 switch (myName.toLowerCase()) {
 case "peter":
@@ -17,12 +22,12 @@ case "peter":
 default:
   alert("Welcome to my about me page " + titleCase(myName) + ".");
 }
-let nameBye =("Thank you for visiting my about page " + titleCase(myName) + ".");
 
 let question1 = confirm("Are you smiling inside today?");
 
 if (question1 === true) {
   alert("Awesome! Never change");
+  correctAnswers++;
 } else {
   alert("That's a shame, I hope you find your inner happy.");
 }
@@ -31,6 +36,7 @@ let question2 = confirm("Are you seated comfortably?");
 
 if (question2 === true) {
   alert("Great, hold that pose!");
+  correctAnswers++;
 } else {
   alert("Have a stretch and get comfy!");
 }
@@ -39,6 +45,7 @@ let question3 = confirm("Are you hydrated?");
 
 if (question3 === true) {
   alert("HYDRO HOMIESSSS!");
+  correctAnswers++;
 } else {
   alert("Fetch a glass of water, dehydration makes mistakes!");
 }
@@ -47,24 +54,17 @@ let question4 = confirm("Have you eaten breakfast?");
 
 if (question4 === true) {
   alert("Good going, you are ready to face the day.");
+  correctAnswers++;
 } else {
   alert("You're not you when you're hungry.");
 }
 
-let question5 = confirm("Are you ready to code?");
-
-if (question5 === true) {
-  alert("Code . and may the bugs be ever in your favour.");
-} else {
-  alert("Maybe I can help with that?");
-}
-
-let day = prompt("What is your favourite day of the week?");
+let question5 = prompt("What is your favourite day of the week?");
 
 // one way of making lowercase
 // colour = colour.toLowerCase();
 
-switch (day.toLowerCase()) {
+switch (question5.toLowerCase()) {
 case "monday":
   alert("Your favourite day is monday? We are not friends.");
   break;
@@ -73,6 +73,7 @@ case "tuesday":
   break;
 case "wednesday":
   alert("It is wednesday my dude.");
+  correctAnswers++;
   break;
 case "thursday":
   alert("Tuesday is THOR'S day!");
@@ -89,4 +90,52 @@ case "sunday":
 default:
   alert("Unknown day, maybe you love them all?");
 }
+
+// Question 6: Numeric input
+let question6 = prompt("What is the answer to Life, The universe, and everything? Hint: Two digit number between 40 and 50.");
+let q6Attempts = 3;
+
+// Check if the answer is correct
+while (question6 != 42 && q6Attempts > 0) {
+  alert("Sorry, that isn't correct. " + q6Attempts + " attempts remaining.");
+  if (question6 < 42) {
+    question6 = prompt("What is the answer to Life, The universe, and everything? Hint: Try guessing a little higher");
+  } else if (question6 > 42) {
+    question6 = prompt("What is the answer to Life, The universe, and everything? Hint: Try guessing a little lower");
+  } else {
+    question6 = prompt("What is the answer to Life, The universe, and everything? Hint: Try guessing a two-digit number");
+  }
+  q6Attempts--;
+}
+
+// increment correctAnswers when correct
+if (question6 == 42) {
+  correctAnswers++;
+  alert("Congratulations, the answer is 42! You had " + q6Attempts + " attempts left.");
+} else {
+  alert("Sorry, you have 0 attempts left. The correct answer is 42!");
+}
+
+// Question 7: Multiple correct answers
+let answers7 = ["red", "blue", "green"];
+let question7 = prompt("Guess one of my favourite colours?").toLowerCase();
+let q7Attempts = 5;
+
+// Check if the answer is correct
+while (!answers7.includes(question7) && q7Attempts > 0) {
+  alert("Sorry, that isn't correct. " + q7Attempts + " attempts remaining.");
+  question7 = prompt("Guess one of my favourite colours? Hint: Try a primary colour.").toLowerCase();
+  q7Attempts--;
+}
+
+// increment correctAnswers when correct
+if (answers7.includes(question7)) {
+  alert("You guessed correctly with "+ q7Attempts + " attempts remaining. Correct answers were: " + answers7.join(", ") + ".");
+  correctAnswers++;
+} else {
+  alert("Sorry, you have no attempts left. Correct answers were: " + answers7.join(", ") + ".");
+}
+let nameBye =("Thank you for visiting my about page " + titleCase(myName) + "You got " + correctAnswers + " out of 7 correct!");
+
+// show score
 alert(nameBye);
