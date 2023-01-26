dcb57f515f85ee62bd73650dd45c552e5c20f774
Peter Brightmore <P.C.Brightmore@gmail.com>
2023-01-26 12:13:47 +0000

    update formatting

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 "use strict";
 function titleCase(str) {
   str = str.split(" ");
-  let title ="";
+  let title = "";
   let result = [];
-  for(let i = 0; i < str.length; i++){
+  for (let i = 0; i < str.length; i++) {
     title = str[i].toLowerCase();
-    result.push(title[0].toUpperCase()+title.slice(1));
+    result.push(title[0].toUpperCase() + title.slice(1));
   }
   return result.join(" ");
 }
@@ -14,7 +14,7 @@ function titleCase(str) {
 let correctAnswers = 0;
 
 // Ask user name
-let myName = prompt ("What is your name?");
+let myName = prompt("What is your name?");
 switch (myName.toLowerCase()) {
 case "peter":
   alert("I like you, " + titleCase(myName) + " I like your face.");
@@ -66,7 +66,9 @@ let question5 = prompt("What is your favourite day of the week?");
 
 switch (question5.toLowerCase()) {
 case "monday":
-  alert("Your favourite day is " + titleCase(question5) + "? We are not friends.");
+  alert(
+    "Your favourite day is " + titleCase(question5) + "? We are not friends."
+  );
   break;
 case "tuesday":
   alert(titleCase(question5) + "? You are clearly full of grace.");
@@ -93,18 +95,26 @@ default:
 console.log("User's favourite day: " + titleCase(question5) + ".");
 
 // Question 6: Numeric input
-let question6 = prompt("What is the answer to Life, The universe, and everything? Hint: Two digit number between 40 and 50.");
+let question6 = prompt(
+  "What is the answer to Life, The universe, and everything? Hint: Two digit number between 40 and 50."
+);
 let q6Attempts = 3;
 
 // Check if the answer is correct
 while (question6 != 42 && q6Attempts > 0) {
   alert("Sorry, that isn't correct. " + q6Attempts + " attempts remaining.");
   if (question6 < 42) {
-    question6 = prompt("What is the answer to Life, The universe, and everything? Hint: Try guessing a little higher.");
+    question6 = prompt(
+      "What is the answer to Life, The universe, and everything? Hint: Try guessing a little higher."
+    );
   } else if (question6 > 42) {
-    question6 = prompt("What is the answer to Life, The universe, and everything? Hint: Try guessing a little lower.");
+    question6 = prompt(
+      "What is the answer to Life, The universe, and everything? Hint: Try guessing a little lower."
+    );
   } else {
-    question6 = prompt("What is the answer to Life, The universe, and everything? Hint: Try guessing a two-digit number.");
+    question6 = prompt(
+      "What is the answer to Life, The universe, and everything? Hint: Try guessing a two-digit number."
+    );
   }
   q6Attempts--;
 }
@@ -112,11 +122,21 @@ while (question6 != 42 && q6Attempts > 0) {
 // increment correctAnswers when correct
 if (question6 == 42) {
   correctAnswers++;
-  alert("Congratulations, the answer is 42! You had " + q6Attempts + " attempts left.");
+  alert(
+    "Congratulations, the answer is 42! You had " +
+      q6Attempts +
+      " attempts left."
+  );
 } else {
   alert("Sorry, you have 0 attempts left. The correct answer is 42!");
 }
-console.log("User's answer to life: " + question6 + ". Attempts remaining: " + q6Attempts + ".");
+console.log(
+  "User's answer to life: " +
+    question6 +
+    ". Attempts remaining: " +
+    q6Attempts +
+    "."
+);
 
 // Question 7: Multiple correct answers
 let answers7 = ["red", "blue", "green"];
@@ -126,20 +146,39 @@ let q7Attempts = 5;
 // Check if the answer is correct
 while (!answers7.includes(question7) && q7Attempts > 0) {
   alert("Sorry, that isn't correct. " + q7Attempts + " attempts remaining.");
-  question7 = prompt("Guess one of my favourite colours? Hint: Try a primary colour.").toLowerCase();
+  question7 = prompt(
+    "Guess one of my favourite colours? Hint: Try a primary colour."
+  ).toLowerCase();
   q7Attempts--;
 }
 
 // increment correctAnswers when correct
 if (answers7.includes(question7)) {
-  alert("You guessed correctly with "+ q7Attempts + " attempts remaining. Correct answers were: " + titleCase(answers7.join(", ")) + ".");
+  alert(
+    "You guessed correctly with " +
+      q7Attempts +
+      " attempts remaining. Correct answers were: " +
+      titleCase(answers7.join(", ")) +
+      "."
+  );
   correctAnswers++;
 } else {
-  alert("Sorry, you have no attempts left. Correct answers were: " + titleCase(answers7.join(", ")) + ".");
+  alert(
+    "Sorry, you have no attempts left. Correct answers were: " +
+      titleCase(answers7.join(", ")) +
+      "."
+  );
 }
-console.log("Colour picked: " + question7 + ". Attempts remaining: " + q7Attempts + ".");
+console.log(
+  "Colour picked: " + question7 + ". Attempts remaining: " + q7Attempts + "."
+);
 console.log(myName + " scored " + correctAnswers + "points out of 7.");
-let nameBye =("Thank you for visiting my about page " + titleCase(myName) + ". You got " + correctAnswers + " out of 7 correct!");
+let nameBye =
+  "Thank you for visiting my about page " +
+  titleCase(myName) +
+  ". You got " +
+  correctAnswers +
+  " out of 7 correct!";
 
 // show score
 alert(nameBye);
